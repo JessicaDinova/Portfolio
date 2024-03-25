4285cfbd3356eaf23a321a78f6dd28ac4bcf098c
Jessica Dinova <dino0001@hz.nl>
2024-03-25 20:15:12 +0100

    scroll into view works for every component

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -3,9 +3,18 @@
     export let image;
     export let title;
     export let link;
+
+    function scrollIntoView() {
+        // @ts-ignore
+        const el = document.querySelector(this.getAttribute("href"));
+        if (!el) return;
+        el.scrollIntoView({
+            behavior: "smooth",
+        });
+    }
 </script>
 
-<a href="{link}" on:click|preventDefault>
+<a href="{link}" on:click|preventDefault={scrollIntoView}>
 <div class="relative w-64 h-[17rem] bg-cream-100 flex {cardOrder ? 'flex-col-reverse' : 'flex-col'} gap-5 px-4 py-2 rounded-lg items-center justify-center hover:scale-105 duration-100 hover:cursor-pointer">
     <div class="text-center">
         <h1 class="text-3xl">{title}</h1>
