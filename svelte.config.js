1104a2bd8f19c3ae3bd3a2d647587e2630740f82
Jessica Dinova <dino0001@hz.nl>
2024-03-29 12:48:49 +0100

    configuration for static building

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,6 +8,9 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
+	},
+	paths: {
+		base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 	},
 	preprocess:vitePreprocess()
 };
